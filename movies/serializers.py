0981488c2d21a7110f524e42f3a1fcb3aa910a83
Danielzooo <104256692+Danielzooo@users.noreply.github.com>
2022-09-22 19:12:20 +0600

    DRF 11

---
+++ movies/serializers.py
@@ -0,0 +1,81 @@
+from rest_framework import serializers
+from .models import Movie, Review, Rating, Actor
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class MovieListSerializer(serializers.ModelSerializer):
+    rating_user = serializers.BooleanField()
+    middle_star = serializers.IntegerField()
+
+    class Meta:
+        model = Movie
+        fields = ('id', 'title', 'tagline', 'category', 'rating_user', 'middle_star')
+
+
+class ReviewCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ActorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ('id', 'name', 'image')
+
+
+class ActorDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    children = RecursiveSerializer(many=True)
+
+    class Meta:
+        list_serializer_class = FilterReviewListSerializer
+        model = Review
+        fields = ('name', 'text', 'children')
+
+
+class MovieDetailSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    directors = ActorListSerializer(read_only=True, many=True)
+    actor = ActorListSerializer(read_only=True, many=True)
+    # genre = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    reviews = ReviewCreateSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        exclude = ('draft', )
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """Добавление рейтинга пользователем"""
+    class Meta:
+        model = Rating
+        fields = ("star", "movie")
+
+    def create(self, validated_data):
+        rating, _ = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get("star")}
+        )
+        return rating
+
